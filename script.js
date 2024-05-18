dd3b3f9a87cb2282815aaf6509669ddf285ee4df
Ankitakamthe153 <88307915+Ankitakamthe153@users.noreply.github.com>
2024-05-18 22:05:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstWord(s) {
   // your code here
+	s = s.trim();
+    if (!s) {
+        return s; // Empty string or only  spaces
+    }
+	
+    // Find the index of the first space
+    const spaceIndex = s.indexOf(' ');
+    if (spaceIndex === -1) {
+        return s; // No space found, return the entire string
+    }
+    return s.slice(0, spaceIndex); // Extract the first word
 }
 
 // Do not change the code below
